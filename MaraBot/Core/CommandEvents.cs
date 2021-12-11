7da85a86ba3f58405cbea2657d9e484059722744
sbz <70881002+sbzappa@users.noreply.github.com>
2021-12-11 18:07:15 -0500

    Added more robustness in error messaging. (#58)

--- MaraBot/Core/CommandEvents.cs
+++ MaraBot/Core/CommandEvents.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
@@ -35,59 +36,83 @@ namespace MaraBot.Core
         /// <returns>Returns an asynchronous task.</returns>
         public static async Task OnCommandErrored(CommandsNextExtension cne, CommandErrorEventArgs e)
         {
-            var formattedCommand = Formatter.InlineCode($"{e.Context.Prefix}{e.Command.Name}");
-            var failedChecks = ((ChecksFailedException)e.Exception).FailedChecks;
-            foreach (var failedCheck in failedChecks)
-            {
-                switch (failedCheck)
-                {
-                    case CooldownAttribute cooldownAttribute:
-                    {
-                        var cooldown = cooldownAttribute.Reset.ToString(@"mm");
-                        var remaining = cooldownAttribute.GetRemainingCooldown(e.Context).ToString(@"mm\:ss");
+            var commandName = e.Command?.Name ?? "[not-found]";
+            var formattedCommand = Formatter.InlineCode($"{e.Context.Prefix}{commandName}");
 
-                        await e.Context.RespondAsync(
-                            $"Command {formattedCommand} is on cooldown for {cooldown} minutes!\n" +
-                            $"{remaining} is remaining!"
+            var helpCommand = $"{e.Context.Prefix}help";
+            var formattedHelpCommand = Formatter.InlineCode(helpCommand);
+
+            if (e.Command == null)
+            {
+                await e.Context.RespondAsync(
+                    "This command does not exist.\n" +
+                    $"Type {formattedHelpCommand} to get a list of all commands."
+                );
+            }
+
+            if (e.Exception is ArgumentException)
+            {
+                await e.Context.RespondAsync(
+                    $"Wrong parameters for command {formattedCommand}.\n" +
+                    $"Type {Formatter.InlineCode(helpCommand + " " + commandName)} for more info."
+                );
+            }
+
+            if (e.Exception is ChecksFailedException cfe)
+            {
+                var failedChecks = cfe.FailedChecks;
+                foreach (var failedCheck in failedChecks)
+                {
+                    switch (failedCheck)
+                    {
+                        case CooldownAttribute cooldownAttribute:
+                        {
+                            var cooldown = cooldownAttribute.Reset.ToString(@"mm");
+                            var remaining = cooldownAttribute.GetRemainingCooldown(e.Context).ToString(@"mm\:ss");
+
+                            await e.Context.RespondAsync(
+                                $"Command {formattedCommand} is on cooldown for {cooldown} minutes!\n" +
+                                $"{remaining} is remaining!"
                             );
-                        break;
-                    }
-                    case RequireBotPermissionsAttribute _:
-                    {
-                        await e.Context.RespondAsync($"Insufficient bot permissions to run command {formattedCommand}.");
-                        break;
-                    }
-                    case RequireUserPermissionsAttribute _:
-                    {
-                        await e.Context.RespondAsync($"{e.Context.User.Mention} has insufficient permissions to run command {formattedCommand}.");
-                        break;
-                    }
-                    case RequireRolesAttribute _:
-                    {
-                        await e.Context.RespondAsync($"{e.Context.User.Mention} does not have a role that can run command {formattedCommand}.");
-                        break;
-                    }
-                    case RequireGuildAttribute _:
-                    {
-                        await e.Context.RespondAsync($"Command {formattedCommand} can only be run in a guild.");
-                        break;
-                    }
-                    case RequireDirectMessageAttribute _:
-                    {
-                        await e.Context.RespondAsync($"Command {formattedCommand} can only be run in direct message to the bot.");
-                        break;
-                    }
-                    default:
-                    {
-                        await e.Context.RespondAsync(
-                            $"Unrecognized failed check {failedCheck.GetType().Name}.\n" +
-                            CommandUtils.kFriendlyMessage);
-                        break;
+                            break;
+                        }
+                        case RequireBotPermissionsAttribute _:
+                        {
+                            await e.Context.RespondAsync($"Insufficient bot permissions to run command {formattedCommand}.");
+                            break;
+                        }
+                        case RequireUserPermissionsAttribute _:
+                        {
+                            await e.Context.RespondAsync($"{e.Context.User.Mention} has insufficient permissions to run command {formattedCommand}.");
+                            break;
+                        }
+                        case RequireRolesAttribute _:
+                        {
+                            await e.Context.RespondAsync($"{e.Context.User.Mention} does not have a role that can run command {formattedCommand}.");
+                            break;
+                        }
+                        case RequireGuildAttribute _:
+                        {
+                            await e.Context.RespondAsync($"Command {formattedCommand} can only be run in a guild.");
+                            break;
+                        }
+                        case RequireDirectMessageAttribute _:
+                        {
+                            await e.Context.RespondAsync($"Command {formattedCommand} can only be run in direct message to the bot.");
+                            break;
+                        }
+                        default:
+                        {
+                            await e.Context.RespondAsync(
+                                $"Unrecognized failed check {failedCheck.GetType().Name}.\n" +
+                                CommandUtils.kFriendlyMessage);
+                            break;
+                        }
                     }
                 }
             }
 
-            string msg = $"Error while executing command [{e.Context.Prefix}{e.Command.Name}] in " + (e.Context.Guild == null
+            string msg = $"Error while executing command [{formattedCommand}] in " + (e.Context.Guild == null
                     ? "direct message:"
                     : $"channel [#{e.Context.Channel.Name}] of guild [{e.Context.Guild.Name}]:");
             cne.Client.Logger.LogError(e.Exception, msg);
