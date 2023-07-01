27ac26a5efa5063cd29ad7f8c73ed6af4d1641f8
sbzappa <sbzappa@gmail.com>
2023-07-01 18:32:35 -0400

    Fixed emojis embedded in time format.

--- MaraBot/Commands/CompletedCommandModule.cs
+++ MaraBot/Commands/CompletedCommandModule.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
@@ -29,7 +30,7 @@ namespace MaraBot.Commands
         /// Executes the completed command.
         /// </summary>
         /// <param name="ctx">Command Context.</param>
-        /// <param name="time">Elapsed time. Expecting HH:MM:SS format.</param>
+        /// <param name="timeRaw">Elapsed time. Expecting HH:MM:SS format.</param>
         /// <returns>Returns an asynchronous task.</returns>
         [Command("completed")]
         [Aliases("done")]
@@ -41,13 +42,22 @@ namespace MaraBot.Commands
             Permissions.ManageMessages |
             Permissions.ManageRoles |
             Permissions.AccessChannels)]
-        public async Task Execute(CommandContext ctx, TimeSpan time)
+        public async Task Execute(CommandContext ctx, [RemainingText]string timeRaw)
         {
             // Give a sufficient delay before deleting message
             await Task.Delay(500);
 
             // Delete user message to avoid spoilers, if we can delete the message.
             await ctx.Message.DeleteAsync();
+
+            // Remove potential emojis embedded in time...
+            timeRaw = Regex.Replace(timeRaw, "<(?<text>(:.+?:)).+?>", "$1");
+
+            if (!TimeSpan.TryParse(timeRaw, out var time))
+            {
+                await ctx.RespondAsync($"Invalid time! Expecting time with a HH:MM:SS format.\n");
+                return;
+            }
 
             // Add user to leaderboard.
             Weekly.AddToLeaderboard(ctx.User.Username, time);
